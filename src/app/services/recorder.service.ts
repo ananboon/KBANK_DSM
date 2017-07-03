0f3e93df7cb50d186821eeaadeb904488e5afab2
Pasakorn Promsuban <pasakorn.p@beryl8.com>
2017-07-03 18:27:25 +0700

    Added user service to send account data and user data to chrome extension

--- src/app/services/recorder.service.ts
+++ src/app/services/recorder.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 
+import { UserService } from '../services/user.service';
 
+import { UserModel } from '../models/user.model';
 import { MessageModel } from '../models/message.model';
 
 import * as RecordRTC from 'recordrtc';
@@ -9,7 +11,7 @@ import * as FileSaver from 'file-saver';
 
 import * as globals from '../globals';
 
-
+@Injectable()
 export class RecorderService{
 
   accountName = '';
@@ -19,7 +21,7 @@ export class RecorderService{
   recording = false;
 
   constructor(
-
+    private userService: UserService
   ){}
 
   setAccountNameAndCIS(accountName, accountCIS){
@@ -40,6 +42,7 @@ export class RecorderService{
       body.message = {
         name: this.accountName,
         CIS: this.accountCIS,
+        userId: this.userService.user.id,
         date : new Date().toLocaleDateString()
       }
     }else if(command === globals.STOP_RECORDING){
